d7756e1663b1346f855ce91af03e688cdfff8348
MohitMic2 <mohit.microlent2@microlent.in>
2024-09-03 17:58:00 +0530

    Change 'referId' to 'referCode'

--- src/components/ReferId/ReferId.jsx
+++ src/components/ReferId/ReferId.jsx
@@ -284,7 +284,7 @@ const ReferId = () => {
 
   const fetchUserCount = async (referId) => {
     const usersCollection = collection(db, 'users');
-    const usersQuery = query(usersCollection, where('referId', '==', referId));
+    const usersQuery = query(usersCollection, where('referCode', '==', referId));  // Change 'referId' to 'referCode'
     const usersSnapshot = await getDocs(usersQuery);
     const userCount = usersSnapshot.docs.length; // Get the count of users using this referId
     
